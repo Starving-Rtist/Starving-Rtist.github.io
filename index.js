3261496951e46e0222880aa62d97799ec0437ac8
Starving-Rtist <89197109+Starving-Rtist@users.noreply.github.com>
2021-10-06 20:16:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BASE_URL = ("Starving-Rtist.github.io") //SWITCHING between "Starving-Rtist.github.io" and "http://localhost:3000/dishes" for web hosting purposes
+const BASE_URL = ("https://starving-rtist.github.io/") //SWITCHING between "https://starving-rtist.github.io/" and "http://localhost:3000/dishes" for web hosting purposes
 
 fetch(BASE_URL)
     .then(resp => resp.json())

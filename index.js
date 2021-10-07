bbe370c01f9f12060a17d75b625326551801d738
Starving-Rtist <89197109+Starving-Rtist@users.noreply.github.com>
2021-10-07 10:10:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BASE_URL = ("https://starving-rtist.github.io/dishes") //SWITCHING between "https://my-json-server.typicode.com/Starving-Rtist/Starving-Rtist.github.io/dishes" and "http://localhost:3000/dishes" for web hosting purposes
+const BASE_URL = ("https://starving-rtist.github.io/index.json") //SWITCHING between "https://my-json-server.typicode.com/Starving-Rtist/Starving-Rtist.github.io/dishes" and "http://localhost:3000/dishes" for web hosting purposes
 
 fetch(BASE_URL)
     .then(resp => resp.json())
